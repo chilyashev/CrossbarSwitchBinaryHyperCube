e531c999c736e4a3805c821c4b2badfd9d8120d0
Mihail Chilyashev <chilyashev@gmail.com>
2015-03-30 12:29:44 +0300

    Stuff done on Flit and stuff

--- src/main/java/org/cbsbh/model/routing/InputChannel.java
+++ src/main/java/org/cbsbh/model/routing/InputChannel.java
@@ -21,19 +21,19 @@ public class InputChannel extends Channel implements Tickable {
      * Начално състояние. По него се влиза след RESET, след INIT,
      * след освобождаване на опашката от вече приет и прочетен пакет пакет или след изтекъл timeout.
      */
-    public static final int STATE_INIT = 0; // S0
+    public static final int STATE0_INIT = 0; // S0
 
-    public static final int STATE_IDLE = 1; // S1
+    public static final int STATE1_IDLE = 1; // S1
 
-    public static final int STATE_READY = 2; // S2
+    public static final int STATE2_READY = 2; // S2
 
-    public static final int STATE_REQUEST_FOR_ROUTING = 3; // S3
+    public static final int STATE3_REQUEST_FOR_ROUTING = 3; // S3
 
-    public static final int STATE_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL = 4; // S4
+    public static final int STATE4_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL = 4; // S4
 
-    public static final int STATE_READ_PACKET = 5; // S5
+    public static final int STATE5_READ_PACKET = 5; // S5
 
-    public static final int STATE_WAIT_FOR_END_OF_PACKET = 6; // S6
+    public static final int STATE6_WAIT_FOR_END_OF_PACKET = 6; // S6
 
 
     ArrayList<FIFOQueue> fifoQueues;
@@ -50,7 +50,7 @@ public class InputChannel extends Channel implements Tickable {
         this.fifoQueueCount = Context.getInstance().getInteger("fifoQueueCount");
         fifoQueues = new ArrayList<>(fifoQueueCount);
 
-        setState(STATE_INIT);
+        setState(STATE0_INIT);
     }
 
     /**
@@ -74,45 +74,45 @@ public class InputChannel extends Channel implements Tickable {
     public int calculateState() {
         // TODO: освен RESET и INIT S0 трябва да се върне и при освобождаване на опашката и изтичане на таймаут.
         if (hasInputSignal(InputSignalArray.RESET) || inputSignalArray.hasSignal(InputSignalArray.INIT)) {
-            return STATE_INIT;
+            return STATE0_INIT;
         }
 
         //  Безусловно се минава от S0 към S1
-        if (state == STATE_INIT) {
-            return STATE_IDLE;
+        if (state == STATE0_INIT) {
+            return STATE1_IDLE;
         }
 
         // Ако се намираме в STATE1 и са налични сигналите FIFO_SELECT и DEMUX_RDY, преминаваме в S2
-        if (state == STATE_IDLE && hasInputSignalsAnd(InputSignalArray.FIFO_SELECT, InputSignalArray.DEMUX_RDY)) {
-            return STATE_READY;
+        if (state == STATE1_IDLE && hasInputSignalsAnd(InputSignalArray.FIFO_SELECT, InputSignalArray.DEMUX_RDY)) {
+            return STATE2_READY;
         }
 
         // Ако сме в STATE2 и са налични сигналите VALID_DATA и HEAD_FLIT, преминаваме в S3
-        if (state == STATE_READY && hasInputSignal(InputSignalArray.VALID_DATA) && fifoQueues.get(activeFIFOIndex).head != null) {
-            return STATE_REQUEST_FOR_ROUTING;
+        if (state == STATE2_READY && hasInputSignal(InputSignalArray.VALID_DATA) && fifoQueues.get(activeFIFOIndex).head != null) {
+            return STATE3_REQUEST_FOR_ROUTING;
         }
 
         // Ако сме в STATE3, преминаваме в S4 безусловно
-        if (state == STATE_REQUEST_FOR_ROUTING) {
-            return STATE_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL;
+        if (state == STATE3_REQUEST_FOR_ROUTING) {
+            return STATE4_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL;
         }
 
         // Ако сме в STATE4
-        if (state == STATE_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL) {
+        if (state == STATE4_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL) {
             // NOT(VALID_DATA) AND NOT(TIME_ONE) AND TAIL_FLIT
             if (!hasInputSignal(InputSignalArray.VALID_DATA) && !hasInputSignal(InputSignalArray.TIME_ONE) && fifoQueues.get(activeFIFOIndex).tail != null) {
-                return STATE_READ_PACKET;
+                return STATE5_READ_PACKET;
             // Timeout
             } else if (hasInputSignal(InputSignalArray.TIME_ONE)) {
-                return STATE_INIT;
+                return STATE0_INIT;
             }
-            return STATE_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL;
+            return STATE4_WRITE_PACKET_AND_WAIT_FOR_OUTPUT_CHANNEL;
         }
 
         // Ако сме в STATE5, може да започне да се чете пакет или вече се чете пакет, ако това е започнало в S4
         // TODO: да се разбере как се разбира дали е започнало предаването. Най-вероятно ще стане в tick()
-        if(state == STATE_READ_PACKET){
-            return STATE_WAIT_FOR_END_OF_PACKET;
+        if(state == STATE5_READ_PACKET){
+            return STATE6_WAIT_FOR_END_OF_PACKET;
         }
         return 0xb00b5;
     }
@@ -123,7 +123,7 @@ public class InputChannel extends Channel implements Tickable {
 
         // Действия, които се извършват преди да се изчисли новото състояние.
         switch (state) {
-            case STATE_READY:
+            case STATE2_READY:
                 // Вземане на първата свободна опашка според B_FIFO_STATUS регистъра
                 activeFIFOIndex = getFirstAvailableQueueIndex();
                 assert activeFIFOIndex < 0 || activeFIFOIndex > fifoQueues.size() : "Invalid active queue index returned. Fuck you!";
@@ -135,10 +135,10 @@ public class InputChannel extends Channel implements Tickable {
         // Вземаме новото състояние
         state = calculateState();
         switch (state) {
-            case STATE_INIT:
-                state = STATE_IDLE;
+            case STATE0_INIT:
+                state = STATE1_IDLE;
                 break;
-            case STATE_READY:
+            case STATE2_READY:
 
                 break;
         }
--- src/main/java/org/cbsbh/model/routing/packet/flit/Flit.java
+++ src/main/java/org/cbsbh/model/routing/packet/flit/Flit.java
@@ -8,26 +8,84 @@ package org.cbsbh.model.routing.packet.flit;
  */
 // Не е сигурно дали това ще се ползва
 // Може да е за метаданни
-public class Flit {
-    enum FlitType{
-        TYPE_NORMAL,
-        TYPE_HEAD,
-        TYPE_TAIL
+public class Flit { // *C
+
+    public static final int FLIT_TYPE_NO_DATA = 0b00;
+    public static final int FLIT_TYPE_HEADER = 0b01;
+    public static final int FLIT_TYPE_TAIL = 0b10;
+    public static final int FLIT_TYPE_BODY = 0b11;
+
+    long flitData; // c*
+
+    /**
+     * Съдържа:
+     * <ul>
+     * <li>битове 0 и 1 - тип на флита</li>
+     * <li>бит 2 - EXTERNAL_CLOCK</li>
+     * <li>бит 3 - VALID_DATA</li>
+     * <li>бит 4 - DATA_ACK</li>
+     * <li>бит 5 - CHANNEL_BUSY (0 - free, 1 - busy)</li>
+     * <li>бит 6 - CHANNEL_LOAD</li>
+     * <li>бит 7 - празен :(</li>
+     * </ul>
+     */
+    short controlByte;
+
+
+    /**
+     * Ако младшите два бита на controlByte-а са:
+     * <ul>
+     * <li>00 - NO_DATA, т.е. празен флит</li>
+     * <li>01 - header флит</li>
+     * <li>10 - tail флит</li>
+     * <li>11 - body</li>
+     * </ul>
+     *
+     * @return типът на флита
+     */
+    public int getFlitType() {
+        return controlByte & 0b11;
     }
 
-    FlitType type;
-
-    long flit; // c*
-
-    public Flit(FlitType type) {
-        this.type = type;
+    public int getExternalClock() {
+        return controlByte & (1 << 2);
     }
 
-    public FlitType getType() {
-        return type;
+    public boolean isDataValid() {
+        return (controlByte & (1 << 3)) != 0;
     }
 
-    public void setType(FlitType type) {
-        this.type = type;
+    /**
+     * @return true if busy. Not true (a.k.a false) - not busy.
+     */
+    public boolean isChannelBusy() {
+        return (controlByte >> 4 & 1) == 1;
+    }
+
+    /**
+     * 0 - Channel is not load
+     * 1 - Channel is very load
+     *
+     * @return true or false
+     */
+    public boolean isChannelUnderHeavyLoad() {
+        return (controlByte >> 5 & 1) == 1;
+    }
+
+    /**
+     * Get the Transport Mask
+     *
+     * @return the Transport Mask
+     */
+    public long getTR() {
+        if(getFlitType() != FLIT_TYPE_HEADER){
+            return -1;
+        }
+        return (flitData & 0xfff0000) >> 0x10;
+    }
+
+
+    public void setTR(long tr) {
+        flitData = ((tr & 0xfff) << 16);
     }
 }
